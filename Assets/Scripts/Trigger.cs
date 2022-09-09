7ba7cc93183bc131577087f1e45277fea2a8bdd6
masonwilson <mew8wjn@virginia.edu>
2022-09-09 10:11:52 -0400

    attempt at triggers
    I added a trigger script but it doesn't quite work yet

---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    public GameObject Child;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Trigger function
+    void OnTriggerEnter(Collider other, GameObject text)
+    {
+        Renderer render = GetComponent<Renderer>();
+        render.material.color = Color.green;
+        Child.SetActive(false);
+    }
+}
